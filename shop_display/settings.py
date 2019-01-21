a4a54613f0c09dcb88ecc0cd5d22d3dd7f0339d8
Michael Skiles <mjrskiles@gmail.com>
2019-01-21 15:35:01 -0600

    Added WorkOrder model.

--- shop_display/settings.py
+++ shop_display/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop_display',
-    'workorder_app',
+    'workorder_app.apps.WorkorderAppConfig',
 ]
 
 MIDDLEWARE = [
--- workorder_app/models.py
+++ workorder_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+class WorkOrder(models.Model):
+    def __str__(self):
+        return self.workorder_number
+
+    workorder_number = models.CharField(max_length=200)
+    machine_size = models.IntegerField(default=0)
+    machine_type = models.CharField(max_length=200)
+    due_date = models.DateField(default=datetime.date.today)
