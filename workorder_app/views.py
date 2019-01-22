fe41cac3ffe2eab4d228e617e8fd7e29561db616
Michael Skiles <mjrskiles@gmail.com>
2019-01-21 22:39:18 -0600

    added simple template script

--- workorder_app/views.py
+++ workorder_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import WorkOrder
 
 def index(request):
-    return render(request, 'workorder_list.html', {})
+    workorders = WorkOrder.objects.order_by('-due_date')
+    context = {'workorders': workorders}
+    return render(request, 'workorder_list.html', context)
 
 def workorder_mockup(request):
     return render(request, 'workorder_list_mockup.html', {})
